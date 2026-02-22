a8c7ec69c5834a785247dffc91fbff3febbc23ac
Chayney <chunchunmaru33333@gmail.com>
2026-02-22 20:47:22 +0900

    バックエンドの構築

--- backend/node/src/index.ts
+++ backend/node/src/index.ts
@@ -15,7 +15,7 @@ export const app = express();
 
 export const start = async () => {
     const PORT = process.env.PORT || 3000;
-    const FRONTEND_URL = process.env.FRONTEND_URL;
+    const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
     console.log(FRONTEND_URL)
 
     try {
